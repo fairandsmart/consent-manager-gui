a433a6705f9dd1a8c46790120063f2e458d687e2
Frédéric Pierre <frederic.pierre@fairandsmart.com>
2020-07-09 10:35:07 +0200

    Add interceptor to show a dialog when back-end is down

--- src/app/interceptors/health.interceptor.spec.ts
+++ src/app/interceptors/health.interceptor.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HealthInterceptor } from './health.interceptor';
+import { MaterialModule } from '../material.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('HealthInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [MaterialModule, NoopAnimationsModule, TranslateModule.forRoot()],
     providers: [
       HealthInterceptor
-      ]
+    ]
   }));
 
   it('should be created', () => {
