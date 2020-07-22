96027938037fba916a374dde03c268e679cf022a
Frédéric Pierre <frederic.pierre@fairandsmart.com>
2020-07-22 17:17:22 +0200

    Fix entry-card component style for entries with long names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { EntriesListSelectComponent } from './entries/entries-list-select/entrie
 import { EntriesListComponent } from './entries/entries-list/entries-list.component';
 import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 import { ClipboardModule } from '@angular/cdk/clipboard';
+import { TextOverflowDirective } from './common/text-overflow.directive';
 
 const keycloakService = new KeycloakService();
 
@@ -84,7 +85,8 @@ registerLocaleData(localeFr, 'fr');
     ThemeComponent,
     FormCreatorComponent,
     ThemesPageComponent,
-    HealthErrorComponent
+    HealthErrorComponent,
+    TextOverflowDirective
   ],
   imports: [
     BrowserModule,
