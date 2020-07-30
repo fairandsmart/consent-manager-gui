b2b194cfb9fa3c9803750234cc47d580c4191b4c
Frédéric Pierre <frederic.pierre@fairandsmart.com>
2020-07-30 11:24:43 +0200

    Unsubscribe onDestroy

--- src/app/conditions-read-only/conditions-read-only.component.ts
+++ src/app/conditions-read-only/conditions-read-only.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ConsentsResourceService } from '../consents-resource.service';
 import { ActivatedRoute } from '@angular/router';
 import { CollectionMethod, ConsentContext, ConsentFormOrientation, ConsentFormType } from '../models';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { combineLatest, EMPTY } from 'rxjs';
+import { combineLatest, EMPTY, Subscription } from 'rxjs';
 import { catchError, mergeMap, tap } from 'rxjs/operators';
 
 @Component({
@@ -11,9 +11,11 @@ import { catchError, mergeMap, tap } from 'rxjs/operators';
   templateUrl: './conditions-read-only.component.html',
   styleUrls: ['./conditions-read-only.component.scss']
 })
-export class ConditionsReadOnlyComponent implements OnInit {
+export class ConditionsReadOnlyComponent implements OnInit, OnDestroy {
 
   public conditionsUrl: SafeResourceUrl;
+
+  private subs: Subscription[] = [];
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -21,43 +23,49 @@ export class ConditionsReadOnlyComponent implements OnInit {
     private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    combineLatest([
-      this.activatedRoute.paramMap,
-      this.activatedRoute.queryParamMap
-    ]).pipe(
-      mergeMap(([params, queryParams]) => {
-        const context: ConsentContext = {
-          owner: params.get('owner'),
-          subject: '',
-          orientation: ConsentFormOrientation.VERTICAL,
-          header: '',
-          elements: [params.get('key')],
-          footer: '',
-          callback: '',
-          validity: '',
-          locale: queryParams.get('lang') ? queryParams.get('lang') : 'en',
-          formType: ConsentFormType.FULL,
-          receiptDeliveryType: 'NONE',
-          userinfos: {},
-          attributes: {},
-          optoutEmail: '',
-          collectionMethod: CollectionMethod.WEBFORM,
-          author: '',
-          preview: true,
-          iframe: true,
-          conditions: true,
-          theme: ''
-        };
-        return this.consentsResource.generateToken(context);
-      }),
-      tap((token) => {
-        this.conditionsUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.consentsResource.getFormUrl(token));
-      }),
-      catchError((error) => {
-        console.error(error);
-        return EMPTY;
-      })
-    ).subscribe();
+    this.subs.push(
+      combineLatest([
+        this.activatedRoute.paramMap,
+        this.activatedRoute.queryParamMap
+      ]).pipe(
+        mergeMap(([params, queryParams]) => {
+          const context: ConsentContext = {
+            owner: params.get('owner'),
+            subject: '',
+            orientation: ConsentFormOrientation.VERTICAL,
+            header: '',
+            elements: [params.get('key')],
+            footer: '',
+            callback: '',
+            validity: '',
+            locale: queryParams.get('lang') ? queryParams.get('lang') : 'en',
+            formType: ConsentFormType.FULL,
+            receiptDeliveryType: 'NONE',
+            userinfos: {},
+            attributes: {},
+            optoutEmail: '',
+            collectionMethod: CollectionMethod.WEBFORM,
+            author: '',
+            preview: true,
+            iframe: true,
+            conditions: true,
+            theme: ''
+          };
+          return this.consentsResource.generateToken(context);
+        }),
+        tap((token) => {
+          this.conditionsUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.consentsResource.getFormUrl(token));
+        }),
+        catchError((error) => {
+          console.error(error);
+          return EMPTY;
+        })
+      ).subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs?.forEach(s => s.unsubscribe());
   }
 
 }
