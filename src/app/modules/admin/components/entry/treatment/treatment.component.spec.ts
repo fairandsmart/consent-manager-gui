e455b5c46fbd574e6fe32b3c55da44f6affdc67e
Alan Balbo <alan.balbo@gmail.com>
2020-10-23 14:16:46 +0200

    Fix tests

--- src/app/modules/admin/components/entry/treatment/treatment.component.spec.ts
+++ src/app/modules/admin/components/entry/treatment/treatment.component.spec.ts
@@ -10,22 +10,26 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AlertService } from '../../../../../core/services/alert.service';
 import SpyObj = jasmine.SpyObj;
 import createSpyObj = jasmine.createSpyObj;
+import { KeycloakService } from 'keycloak-angular';
 
 describe('TreatmentComponent', () => {
   let component: TreatmentComponent;
   let fixture: ComponentFixture<TreatmentComponent>;
   let modelsResourceServiceSpy: SpyObj<ModelsResourceService>;
+  let keycloakServiceSpy: SpyObj<KeycloakService>;
   let alertServiceSpy: SpyObj<AlertService>;
 
   beforeEach(waitForAsync(() => {
     modelsResourceServiceSpy =  createSpyObj<ModelsResourceService>('ModelsResourceService', ['listEntries']);
     alertServiceSpy =  createSpyObj('AlertService', ['error']);
+    keycloakServiceSpy =  createSpyObj<KeycloakService>('KeycloakService', ['getUsername']);
 
     TestBed.configureTestingModule({
       declarations: [ TreatmentComponent, EntryInfoComponent, EntryPreviewComponent ],
       imports: [ CoreTestingModule, RouterTestingModule, ReactiveFormsModule ],
       providers: [
         {provide: ModelsResourceService, useValue: modelsResourceServiceSpy},
+        {provide: KeycloakService, useValue: keycloakServiceSpy},
         {provide: AlertService, useValue: alertServiceSpy}
       ]
     })
