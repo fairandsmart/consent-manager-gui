d32196424f62680845e51fcfc3b4e191b4b98e8c
Frédéric Pierre <frederic.pierre@fairandsmart.com>
2020-11-17 17:53:36 +0100

    Fix tests

--- src/app/modules/admin/pages/getting-started-page/getting-started-page.component.spec.ts
+++ src/app/modules/admin/pages/getting-started-page/getting-started-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GettingStartedPageComponent } from './getting-started-page.component';
+import { CoreTestingModule } from '../../../../testing/core-testing-module.spec';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('GettingStartedPageComponent', () => {
   let component: GettingStartedPageComponent;
@@ -8,7 +12,8 @@ describe('GettingStartedPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GettingStartedPageComponent ]
+      declarations: [ GettingStartedPageComponent ],
+      imports: [ CoreTestingModule, RouterTestingModule, NoopAnimationsModule, TranslateModule.forRoot() ],
     })
     .compileComponents();
   });
