53e571408a72d24557f5f3aa7d0130a8d0ce2f02
Alan Balbo <alan.balbo@gmail.com>
2020-11-26 17:01:45 +0100

    PRD-1165 Added receipt format in context

--- src/app/core/models/models.ts
+++ src/app/core/models/models.ts
@@ -252,6 +252,7 @@ export interface ConsentContext {
   validity?: string;
   formType: ConsentFormType;
   receiptDeliveryType: ReceiptDeliveryType;
+  receiptDisplayType?: ReceiptDisplayType;
   userinfos: { [key: string]: string };
   attributes: { [key: string]: string };
   notificationModel: string;
@@ -283,7 +284,10 @@ export enum CollectionMethod {
 
 export type ReceiptDeliveryType = 'NONE' | 'GENERATE' | 'DISPLAY' | 'STORE' | 'DOWNLOAD';
 
+export type ReceiptDisplayType = 'HTML' | 'XML' | 'PDF' | 'TEXT';
+
 export const RECEIPT_DELIVERY_TYPES: ReceiptDeliveryType[] = ['NONE', 'GENERATE', 'DISPLAY', 'STORE', 'DOWNLOAD'];
+export const RECEIPT_DISPLAY_TYPES: ReceiptDisplayType[] = ['HTML', 'XML', 'PDF', 'TEXT'];
 
 /* Records */
 
