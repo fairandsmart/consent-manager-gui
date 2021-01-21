0e4b49a0bd324fc953caea6cb8c9bce7c9061e87
Nicolas Rueff <nicolas@enere.fr>
2021-01-21 15:00:11 +0100

    License header update

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,3 +1,18 @@
+/*-
+ * #%L
+ * Right Consents, a Universal Consents & Preferences Management Platform.
+ * %%
+ * Copyright (C) 2020 - 2021 Fair And Smart
+ * %%
+ * This file is part of Right Consents Community Edition.
+ * 
+ * Right Consents Community Edition is published by FAIR AND SMART under the
+ * GNU GENERAL PUBLIC LICENCE Version 3 (GPLv3) and a set of additional terms.
+ * 
+ * For more information, please see the “LICENSE” and “LICENSE.FAIRANDSMART”
+ * files, or see https://www.fairandsmart.com/opensource/.
+ * #L%
+ */
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
--- src/app/modules/admin/pages/Integration/interrogate-page/interrogate-page.component.spec.ts
+++ src/app/modules/admin/pages/Integration/interrogate-page/interrogate-page.component.spec.ts
@@ -1,3 +1,18 @@
+/*-
+ * #%L
+ * Right Consents, a Universal Consents & Preferences Management Platform.
+ * %%
+ * Copyright (C) 2020 - 2021 Fair And Smart
+ * %%
+ * This file is part of Right Consents Community Edition.
+ * 
+ * Right Consents Community Edition is published by FAIR AND SMART under the
+ * GNU GENERAL PUBLIC LICENCE Version 3 (GPLv3) and a set of additional terms.
+ * 
+ * For more information, please see the “LICENSE” and “LICENSE.FAIRANDSMART”
+ * files, or see https://www.fairandsmart.com/opensource/.
+ * #L%
+ */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InterrogatePageComponent } from './interrogate-page.component';
