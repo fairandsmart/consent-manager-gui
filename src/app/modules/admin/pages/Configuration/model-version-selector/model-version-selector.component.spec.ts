c9f589402a3d8c79a559fbb9c7846094f640a71f
Alan Balbo <alan.balbo@gmail.com>
2021-02-18 15:16:47 +0100

    PRD-1309 Added missing ModelVersionSelector component

---
+++ src/app/modules/admin/pages/Configuration/model-version-selector/model-version-selector.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModelVersionSelectorComponent } from './model-version-selector.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { CoreTestingModule } from '../../../../../testing/core-testing-module.spec';
+
+describe('ModelVersionSelectorComponent', () => {
+  let component: ModelVersionSelectorComponent;
+  let fixture: ComponentFixture<ModelVersionSelectorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ModelVersionSelectorComponent ],
+      imports: [ CoreTestingModule ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModelVersionSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/admin/pages/Configuration/model-version-selector/model-version-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cm-model-version-selector',
+  templateUrl: './model-version-selector.component.html',
+  styleUrls: ['./model-version-selector.component.scss']
+})
+export class ModelVersionSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
